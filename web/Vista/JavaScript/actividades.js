32a12ea204bf6aff5d0a23a889812464f84d9c69
Paula Gisedt Vanegas Aponte <gisedtvanegas2008@gmail.com>
2026-07-21 08:46:17 -0500

    Delete web directory

--- web/Vista/JavaScript/actividades.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll(".tarjeta-actividad-boton").forEach(function (tarjeta) {
-        tarjeta.addEventListener("click", function () {
-            const girada = tarjeta.classList.toggle("esta-girada");
-            tarjeta.setAttribute("aria-pressed", girada ? "true" : "false");
-        });
-    });
-});
